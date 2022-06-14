86b758c1a864f7e623b6768853660fb49f264e08
tjayling <tjayling@yahoo.co.uk>
2022-06-14 16:02:58 +0100

    Final commit

--- blackjack.js
+++ blackjack.js
@@ -7,8 +7,8 @@ const startButton = document.getElementById("start-button");
 const drawButton = document.getElementById("draw-button");
 const foldButton = document.getElementById("fold-button");
 const resetButton = document.getElementById("reset-button");
-const player1NameContainer = document.getElementById("player1-name-container")
-const player2NameContainer = document.getElementById("player2-name-container")
+const player1NameContainer = document.getElementById("player1-name-container");
+const player2NameContainer = document.getElementById("player2-name-container");
 const player1TotalContainer = document.getElementById(
   "player1-total-container"
 );
@@ -20,6 +20,7 @@ const startButtonContainer = document.getElementById("start-button-container");
 const otherButtonsContainer = document.getElementById(
   "other-buttons-container"
 );
+const winOrLoseContainer = document.getElementById("win-or-lose-container");
 
 let running = false;
 let total = 0;
@@ -49,6 +50,7 @@ function startGame() {
   player2 = new Player("Kevin", false);
   addNames(nameInput.value, "Kevin");
   nameInput.style.display = "none";
+  winOrLoseContainer.innerHTML = ``;
   toggleButtons();
 
   for (let i = 0; i < 2; i++) {
@@ -75,21 +77,21 @@ function drawCard() {
   }
 }
 
-function getCard() {
-  return new Card(Math.floor(Math.random() * 52) + 1);
-}
-
-function addNames(player1Name, player2Name) {
-    let player1NameH2 = document.createElement(`h2`).appendChild(document.createTextNode(player1Name));
-    let player2NameH2 = document.createElement(`h2`).appendChild(document.createTextNode(player2Name));
-    player1NameContainer.replaceChild(player1NameH2, player1NameContainer.childNodes[0])
-    player2NameContainer.replaceChild(player2NameH2, player2NameContainer.childNodes[0])
-}
-
 function foldGame() {
   if (!running) return;
   running = false;
   gameOver(true);
+}
+
+function gameOver(fold) {
+  running = false;
+  let score = `Your total was: ${player1.getTotal()}, you were ${21 - player1.getTotal()} points away from 21`;
+  let win =
+    player1.total > player2.total ? "You beat the bot!" : `The bot beat you!`;
+  let lose = `You went over`;
+  winOrLoseContainer.innerHTML = `<h2>${
+    fold ? `${score}<br/>${win}` : lose
+  }</h2>`;
 }
 
 function resetPage() {
@@ -101,12 +103,25 @@ function resetPage() {
   updateTotal();
 }
 
-function clearList() {}
+function getCard() {
+  return new Card(Math.floor(Math.random() * 52) + 1);
+}
 
-function gameOver(fold) {
-  let win = `Total is: ${total}, you were ${21 - total} points away from 21`;
-  let lose = `You went over`;
-  console.log(fold ? win : lose);
+function addNames(player1Name, player2Name) {
+  let player1NameH2 = document
+    .createElement(`h2`)
+    .appendChild(document.createTextNode(player1Name));
+  let player2NameH2 = document
+    .createElement(`h2`)
+    .appendChild(document.createTextNode(player2Name));
+  player1NameContainer.replaceChild(
+    player1NameH2,
+    player1NameContainer.childNodes[0]
+  );
+  player2NameContainer.replaceChild(
+    player2NameH2,
+    player2NameContainer.childNodes[0]
+  );
 }
 
 function updateTotal() {
